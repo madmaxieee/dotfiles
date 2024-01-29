44ba75077493d278f3ccd9affd5c632c01546b11
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2024-01-29 22:21:24 +0800

    update skhd and yabai config

--- dot_config/skhd/executable_focus_window_in_space.sh
+++ dot_config/skhd/executable_focus_window_in_space.sh
@@ -9,7 +9,6 @@ space_has_focus=$(yabai -m query --windows --space "$space_index" \
 if [[ "$space_has_focus" = "false" ]]; then
     first_window_id="null"
     first_window_id=$(yabai -m query --windows --space "$space_index" | jq '[ .[] | select(.app=="Arc" and .["is-floating"] | not) ].[0].id')
-    yabai -m query --windows --space "$space_index" | jq '[ .[] | select(.app=="Arc" and .["is-floating"] | not) ].[0]'
     if [[ "$first_window_id" != "null" ]]; then
         yabai -m window --focus "$first_window_id"
     else
