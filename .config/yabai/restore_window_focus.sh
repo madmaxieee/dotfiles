fd027c414b78bb8b68e35175cc2fb34f50877d45
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2024-04-02 14:45:43 +0800

    add yabai and skhd

---
+++ .config/yabai/restore_window_focus.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# focus a space while ignoring Arc PIP windows
+
+current_space=$(yabai -m query --spaces | jq '[ .[] | select(."has-focus")][0].id')
+
+~/.config/yabai/focus_window_in_space.sh "$current_space" || exit 1
