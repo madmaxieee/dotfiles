57ee74cb9124f23e47a304f7e3e27e668855bb36
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2024-04-03 23:31:38 +0800

    use gnu pass for openai api keys

--- .config/fish/functions/modsq.fish
+++ .config/fish/functions/modsq.fish
@@ -1,4 +1,4 @@
 function modsq -d "Run a command with the openai API key set" --wraps mods
-    set -x OPENAI_API_KEY (op item get openai_api_key --fields credential)
+    set -x OPENAI_API_KEY (pass openai/aichat)
     mods $argv
 end
