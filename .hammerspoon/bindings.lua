dc0a5885b282e7e47b9d54131805f7d000bceecf
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2024-05-15 01:45:26 +0800

    fix app watcher

--- .hammerspoon/bindings.lua
+++ .hammerspoon/bindings.lua
@@ -151,7 +151,7 @@ app_bind("Messenger", { "cmd" }, "w", function()
 end)
 
 ---@diagnostic disable-next-line: unused-local
-local app_watcher = hs.application.watcher.new(function(app_name, event_type, app)
+App_watcher = hs.application.watcher.new(function(app_name, event_type, app)
     if event_type == hs.application.watcher.activated then
         for name, handlers in pairs(app_bind_handlers) do
             if name == app_name then
@@ -165,9 +165,12 @@ local app_watcher = hs.application.watcher.new(function(app_name, event_type, ap
             end
         end
     elseif event_type == hs.application.watcher.deactivated then
+        if app_bind_handlers[app_name] == nil then
+            return
+        end
         for _, handler in ipairs(app_bind_handlers[app_name]) do
             handler:disable()
         end
     end
 end)
-app_watcher:start()
+App_watcher:start()
