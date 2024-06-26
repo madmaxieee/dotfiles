062e18267a9df578eda1f70e2fc6d6ed5900aa40
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2024-06-27 01:10:44 +0800

    use new project api key from openai for mods

--- .config/fish/functions/modsq.fish
+++ .config/fish/functions/modsq.fish
@@ -1,4 +1,4 @@
 function modsq -d "Run a command with the openai API key set" --wraps mods
-    set -x OPENAI_API_KEY (pass openai/aichat)
+    set -x OPENAI_API_KEY (pass openai/mods)
     mods $argv
 end
