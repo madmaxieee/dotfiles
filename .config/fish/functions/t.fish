2378d5c4010eebe808b045047954453a27a9c325
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2024-08-10 14:25:24 +0800

    manage vipe with fisher

---
+++ .config/fish/functions/t.fish
@@ -0,0 +1,4 @@
+function t --wraps=/Users/madmax/.config/tmux/scripts/sesh.sh --description 'alias t /Users/madmax/.config/tmux/scripts/sesh.sh'
+  /Users/madmax/.config/tmux/scripts/sesh.sh $argv
+        
+end
